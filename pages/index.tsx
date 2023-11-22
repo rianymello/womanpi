335a79bcceef293ae36bcff36175cc290a80d330
Riany Mello <mello.riany@gmail.com>
2023-11-22 19:18:28 +0000

    updating header styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
 import Image from 'next/image';
 import styles from '../styles/index.module.css';
-import headerimg from '../public/headerimg.png'
 import LineLeft from '../assets/lineLeft.png';
 import LineRigth from '../assets/lineRigth.png';
+import WomanImg from '../assets/womanimg.png';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Promoting STEM</h1>
-      <div>
-      <img
-        src="../assets/lineLeft.png"
-        alt="Image"
-      />
-      <h1>under sustentability</h1>
+    <>
+    <div className={styles.headerContainer}>
+      <div className={styles.titleContainer}>
+
+        <h1>Promoting STEM</h1>
+        <div className={styles.underContainer}>
+        <Image src={LineLeft} alt="Line Icon" width={30} height={50} /> 
+        <h1 className={styles.font}>under sustentability</h1>
+        <Image src={LineRigth} alt="Line Icon" width={30} height={50} /> 
+        </div>
+        <h1>for equality</h1>
+
+
       </div>
-      <h1>for equality</h1>
+      <Image src={WomanImg} alt="Woman working" width={400} height={500} className={styles.womanImg}/> 
+
     </div>
+
+    </>
   );
 }
